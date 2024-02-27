a22df0a855b8c02f5817d318f3b60ab50422a4a8
ashok parihar <aparihar@email.arizona.edu>
2024-02-26 20:04:33 -0700

    Adding the screenshots and adjusting variables in the js file.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,10 +3,10 @@ var key = '33304151a3b71e75b3669711447a8267';
 var city = "Phoenix"
 
 // Setting the Variables for the Current Date and Time
-var date = moment().format('dddd, MMMM Do YYYY');
+var currentDate = moment().format('dddd, MMMM Do YYYY');
 var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
 
-console.log(date);
+console.log(currentDate);
 console.log(dateTime);
 
 var cityHistory = [];
@@ -26,9 +26,9 @@ $('.search').on("click", function (event) {
 });
 
 // Generating the previous cities Search History Buttons
-var searchHistEl = $('.cityHistory');
+var searchHistoryEl = $('.cityHistory');
 function getHistory() {
-	searchHistEl.empty();
+	searchHistoryEl.empty();
 
 	for (let i = 0; i < cityHistory.length; i++) {
 
@@ -40,7 +40,7 @@ function getHistory() {
 		btnEl.addClass('btn btn-outline-secondary histBtn');
 		btnEl.attr('type', 'button');
 
-		searchHistEl.prepend(rowEl);
+		searchHistoryEl.prepend(rowEl);
 		rowEl.append(btnEl);
 	} if (!city) {
 		return;
@@ -55,15 +55,15 @@ function getHistory() {
 
 };
 
-var cardToday = $('.cardBody')
-// API Request for specific data and displays that data on a card
+var weatherCardToday = $('.cardBody')
+// API Request for weather specific data and subsequently displays that data on a card
 function getWeatherToday() {
 
     // API Base URL given for the assignment - didn't work!
     // var getUrlCurrent = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`;
 	var getUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`;
 
-	$(cardToday).empty();
+	$(weatherCardToday).empty();
 
 	$.ajax({
 		url: getUrlCurrent,
@@ -71,21 +71,21 @@ function getWeatherToday() {
 	}).then(function (response) {
         // Generating the temperature data card
 		$('.cardTodayCity').text(response.name);
-		$('.cardTodayDate').text(date);
+		$('.cardTodayDate').text(currentDate);
 		$('.icons').attr('src', `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`);
-		// Temperature element
-		var pEl = $('<p>').text(`Temperature: ${response.main.temp} °F`);
-		cardToday.append(pEl);
-        // Wind Speed element
+		// Temperature element for the data card
+		var pElTemp = $('<p>').text(`Temperature: ${response.main.temp} °F`);
+		weatherCardToday.append(pElTemp);
+        // Wind Speed element for the data card
 		var pElWind = $('<p>').text(`Wind Speed: ${response.wind.speed} MPH`);
-		cardToday.append(pElWind);
-		// Humidity element
+		weatherCardToday.append(pElWind);
+		// Humidity element for the data card
 		var pElHumid = $('<p>').text(`Humidity: ${response.main.humidity} %`);
-		cardToday.append(pElHumid);
+		weatherCardToday.append(pElHumid);
     });
     };
 
-// Data will appear inside the weather card for the previous cities entered.
+// Data will appear inside the weather card for the previous cities entered when they are clicked on a second time.
 function initLoad() {
 
 	var cityHistoryStore = JSON.parse(localStorage.getItem('city'));
